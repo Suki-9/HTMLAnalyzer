bd273461d59aee5d300d2bc6ac0f360d1fd95a2a
suki-9 <suki.music2004@gmail.com>
2023-10-11 17:53:03 +0900

    pnpm format

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 // Vue Component ---------------------------------------///
-import App from './App.vue'
+import App from "./App.vue";
 
 // TS Module -------------------------------------------///
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
 // Plugin ----------------------------------------------///
-import router from "./router"
+import router from "./router";
 
 // Style sheet -----------------------------------------///
-import './style/main.css'
+import "./style/main.css";
 
-
-createApp(App)
-  .use(router)
-  .mount('#app')
+createApp(App).use(router).mount("#app");
